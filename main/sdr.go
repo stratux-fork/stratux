9dbc3f9721c7b47b071ac0501873be4fb2dba085
Joseph Poirier <jdpoirier@gmail.com>
2016-03-15 01:25:40 -0500

    sdr: add copyright info

--- main/sdr.go
+++ main/sdr.go
@@ -1,5 +1,6 @@
 /*
 	Copyright (c) 2015-2016 Christopher Young
+	Copyright (c) 2015-2016 Joseph D Poirier
 	Distributable under the terms of The "BSD New"" License
 	that can be found in the LICENSE file, herein included
 	as part of this header.
